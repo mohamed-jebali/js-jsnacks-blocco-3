d4ed3079ef75121b1aa78c9a234a6d46bf14c1da
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-06-15 12:54:30 +0200

    added AppHeader

---
+++ vue3-header/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<script>
+import { createApp } from 'vue'
+export default{
+    data(){
+        return
+    },
+    methods:{
+
+    }
+}
+
+</script>
+
+<style lang="scss">
+
+    
+</style>
